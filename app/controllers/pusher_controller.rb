2623dfc2b2308aebcfa42c2ece2529730060c96f
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-01 19:01:14 +0900

    Pusherでチャット
    ```
    $ vim app/controllers/pusher_controller.rb
    $ vim app/views/pusher/index.html.haml
    $ vim config/initializers/pusher.rb
    $ vim config/routes.rb
    ```

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -1,4 +1,11 @@
 class PusherController < ApplicationController
   def index
+    Pusher.url = ENV["PUSHER_URL"]
+  end
+
+  def create
+    render :nothing => true
+    Pusher['test_channel'].trigger('chat_event', { message: params[:data] })
+    flash.keep[:notice] = "Your password has been updated successfully."
   end
 end
