4da6d7eedba516ddbc33e974aaada88a63bd3f0b
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-04 18:04:30 +0900

    Be able to clear chat history
    ```
    $ vim app/controllers/pusher_controller.rb
    $ vim app/views/pusher/index.html.haml
    $ vim config/routes.rb
    ```

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -10,4 +10,9 @@ class PusherController < ApplicationController
     view_message = { date: message.created_at, content: message.content.gsub(/\r\n|\r|\n/, "<br />") }
     Pusher['test_channel'].trigger('chat_event', { message: view_message })
   end
+
+  def clear
+    Message.delete_all
+    redirect_to action: "index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pusher#index'
   post '/pusher/create', to: 'pusher#create'
+  post '/pusher/clear',  to: 'pusher#clear'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
