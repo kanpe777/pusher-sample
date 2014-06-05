978d3c4786987f4a9fcad13262c4c3f5d8b608c4
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-05 19:59:40 +0900

    Add index to user table name column
    ```
    $ rails g migration AddIndexToUserName
    $ bundle exec rake db:migrate
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605093708) do
+ActiveRecord::Schema.define(version: 20140605105507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,5 +35,7 @@ ActiveRecord::Schema.define(version: 20140605093708) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
 end
