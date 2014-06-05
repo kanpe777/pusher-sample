e08b9366697a9d1e86c71c667c04e3494655ac13
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-05 17:42:23 +0900

    Create custom error page, 404, 500
    ```
    $ mkdir app/views/errors/
    $ vim app/controllers/application_controller.rb
    $ vim app/views/errors/error_404.html.haml
    $ vim app/views/errors/error_500.html.haml
    ```

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,18 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::RoutingError,  with: :render_404
   rescue_from ActiveRecord::RecordNotFound,    with: :render_404
 
-  def render_404
-    render file: "#{Rails.root}/public/404.html", :status => 404, :layout => false, :content_type => 'text/html'
+  def render_404(exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
+    end
+    render template: "errors/error_404", :status => 404, :layout => 'application', :content_type => 'text/html'
+  end
+
+  def render_500(exception = nil)
+    if exception
+      logger.info "Rendering 500 with exception: #{exception.message}"
+    end
+
+    render :template => "errors/error_500", :status => 500, :layout => 'application'
   end
 end
