e9a49fb689d35c00fc107614b350d5ce7ef0a0ee
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-05 12:09:21 +0900

    Be able to delete chat room
    ```
    $ vim app/views/chat_rooms/index.html.haml
    $ vim config/routes.rb
    ```

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'pusher#index'
 
-  get  '/chat_rooms',     to: 'chat_rooms#index'
-  post '/chat_rooms',     to: 'chat_rooms#create'
-  get  '/chat_rooms/:id', to: 'chat_rooms#room'
-  post '/chat_rooms/new', to: 'chat_rooms#new'
+  get    '/chat_rooms',     to: 'chat_rooms#index'
+  post   '/chat_rooms',     to: 'chat_rooms#create'
+  get    '/chat_rooms/:id', to: 'chat_rooms#room'
+  delete '/chat_rooms/:id', to: 'chat_rooms#destroy'
+  post   '/chat_rooms/new', to: 'chat_rooms#new'
 
   post '/pusher/create',  to: 'pusher#create'
   post '/pusher/clear',   to: 'pusher#clear'
