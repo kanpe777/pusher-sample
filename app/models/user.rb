dd5fa05cc176967c45e7822cab53e3871b162738
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-05 19:48:11 +0900

    Add validation user model
    ```
    $ vim app/models/user.rb
    ```
    
    - User.name's length is 20 or less than 20
    - User.name is uniqueness
    - password's length is 4 or greater than 4, and 20 or less than 20

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name,     presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 4, maximum: 20 }
 end
