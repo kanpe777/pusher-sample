cc71c56c5598f4915cee34289a6a98fd9a838135
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-08 16:28:12 +0900

    Be able to see private or public on /chat_rooms
    ```
    $ rails g migration AddPrivateToChatRooms
    $ vim db/migrate/20140608071902_add_private_to_chat_rooms.rb
    $ bundle exec rake db:migrate
    $ vim app/views/chat_rooms/index.html.haml
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607095254) do
+ActiveRecord::Schema.define(version: 20140608071902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140607095254) do
     t.json     "chat"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "private",    default: false
   end
 
   create_table "friend_ships", force: true do |t|
